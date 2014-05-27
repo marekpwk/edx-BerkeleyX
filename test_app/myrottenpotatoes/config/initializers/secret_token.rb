ea660e6cc9deea84c06903a4b65900169018783d
Mar <rkx1901@gmail.com>
2014-05-27 16:59:02 -0500

    Initialized test app

---
+++ test_app/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'b90fa25095b6f4611a1514fcd1e9cc2bc139c1a449d11b0bbf2c6ee7ac1b934c2dc6896c44af80506c48cd865a2004bee164e96d0789d3977b982faf55b872b6'
